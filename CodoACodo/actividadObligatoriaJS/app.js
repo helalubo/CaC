0498279d95566f1b0e98bea9a44780b55b8e083f
Helalubo <alejandrodemoraiz@gmail.com>
2021-05-24 21:04:02 -0300

    ejercicio de js obligatorio

---
+++ CodoACodo/actividadObligatoriaJS/app.js
@@ -0,0 +1,7 @@
+var n1 = 40;
+var n2 = 16;
+var sumaTotal = n1 + n2;
+var respuesta = document.getElementById("resultado");
+var h1 = document.createElement("h1");
+h1.textContent = sumaTotal.toString();
+respuesta.append(h1);
